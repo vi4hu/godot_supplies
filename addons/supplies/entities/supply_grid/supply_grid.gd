3e29ac61a335e1d0b2c1f2036ae2b160b282274a
vi4hu <rainkun3@protonmail.com>
2023-05-01 02:06:20 +0530

    item drag and drop, grid saves supplies

--- addons/supplies/entities/supply_grid/supply_grid.gd
+++ addons/supplies/entities/supply_grid/supply_grid.gd
@@ -5,45 +5,48 @@ extends Area2D
 @onready var collision: CollisionShape2D = $Collision
 @onready var bg: ColorRect = $BG
 
-@export var grid_size : Vector2 = Vector2(64, 64) :
+@export var grid_dimension : Vector2 = Vector2(4, 4) :
 	set (value):
-		grid_size = _handle_grid_size(value)
+		grid_dimension = _handle_grid_size(value)
 
+const grid_size : Vector2 = Vector2(32, 32)
 var is_ready: bool = false
-
+var supply_slots: Dictionary
+var supply_items: Dictionary
 
 func _ready() -> void:
 	is_ready = true
 	_setup()
 	fill_supplies()
+	initiate()
 
 
 func _setup() -> void:
-	_setup_collision(grid_size)
-	_set_bg(grid_size)
-	_setup_grid(grid_size)
+	_setup_collision(grid_dimension)
+	_set_bg(grid_dimension)
+	_setup_grid(grid_dimension)
 
 
-func _setup_grid(_grid_size: Vector2) -> void:
+func _setup_grid(value: Vector2) -> void:
 	grid.set_centered(false)
 	grid.set_region_enabled(true)
-	grid.set_region_rect(Rect2(0, 0, _grid_size.x, _grid_size.y))
+	grid.set_region_rect(Rect2(0, 0, (grid_size.x * value.x), (grid_size.y * value.y)))
 	grid.set_texture_repeat(2)
 
 
-func _setup_collision(_grid_size: Vector2) -> void:
+func _setup_collision(value: Vector2) -> void:
 	collision.set_position(
-		Vector2(_grid_size.x/2, _grid_size.y/2)
+		Vector2((grid_size.x * value.x)/2, (grid_size.y * value.y)/2)
 	)
 	collision.shape.set_size(
-		Vector2(_grid_size.x, _grid_size.y)
+		Vector2((grid_size.x * value.x), (grid_size.y * value.y))
 	)
 
 
-func _set_bg(_grid_size: Vector2) -> void:
+func _set_bg(value: Vector2) -> void:
 	bg.set_color(Color(0.3, 0.3, 0.3))
 	bg.set_position(Vector2.ZERO)
-	bg.set_size(Vector2(_grid_size.x, _grid_size.y))
+	bg.set_size(Vector2((grid_size.x * value.x), (grid_size.y * value.y)))
 
 
 func _handle_grid_size(value: Vector2) -> Vector2:
@@ -56,3 +59,47 @@ func _handle_grid_size(value: Vector2) -> Vector2:
 
 func fill_supplies() -> void:
 	print(SuppliesData.get_supplies())
+
+
+func initiate() -> void:
+	var supplyitems = get_tree().get_nodes_in_group('supplyitem')
+	for supplyitem in supplyitems:
+#		connect('area_entered', supplyitem.inside_inventory)
+#		connect('area_exited', supplyitem.outside_inventory)
+		supplyitem.initiate()
+
+
+func add_item_to_inventory(supply: Control) -> bool:
+	var supply_grid_pos = supply.global_position / grid_size
+	var slot_id: Vector2 = supply_grid_pos - global_position / grid_size
+	var slot_size: Vector2 = supply.size / grid_size
+	
+	var max_supply_grid_pos: Vector2 = supply_grid_pos + slot_size - Vector2(1, 1)
+	var min_slot_bounds: Vector2 = global_position / grid_size - Vector2(1, 1)
+	var max_slot_bounds: Vector2 = min_slot_bounds + grid_dimension
+	
+	if supply_grid_pos.x < min_slot_bounds.x or max_supply_grid_pos.x > max_slot_bounds.x:
+		return false
+
+	if supply_grid_pos.y < min_slot_bounds.y or max_supply_grid_pos.y > max_slot_bounds.y:
+		return false
+#	print(slot_id, slot_size, supply_grid_pos, max_supply_grid_pos, min_slot_bounds, max_slot_bounds)
+		
+	if supply_items.has(supply):
+		remove_item_in_inventory_slot(supply, supply_items[supply])
+
+	for y_ctr in range(slot_size.y):
+		for x_ctr in range(slot_size.x):
+			supply_slots[Vector2(slot_id.x + x_ctr, slot_id.y + y_ctr)] = supply
+
+	supply_items[supply] = slot_id
+	return true
+
+
+func remove_item_in_inventory_slot(supply: Control, existing_id: Vector2):
+	var slot_size: Vector2 = supply.size / grid_size
+
+	for y_Ctr in range(slot_size.y):
+		for x_Ctr in range(slot_size.x):
+			if supply_slots.has(Vector2(existing_id.x + x_Ctr, existing_id.y + y_Ctr)):
+				supply_slots.erase(Vector2(existing_id.x + x_Ctr, existing_id.y + y_Ctr))
--- addons/supplies/entities/supply_item/supply_item.gd
+++ addons/supplies/entities/supply_item/supply_item.gd
@@ -6,16 +6,36 @@ extends Control
 @export var supply_img: Texture2D = preload("res://addons/supplies/resources/grid.png") :
 	set (value):
 		supply_img = _set_image(value)
-@export var dimension: Vector2 = Vector2(32, 32):
+@export var dimension: Vector2 = Vector2(1, 1):
 	set (value):
 		dimension = _set_dimension(value)
 
+const item_size: Vector2 = Vector2(32, 32)
+const pixel_to_leave: Vector2 = Vector2(2, 2)
+
 var uuid: String
 var isready: bool = false
+var isinsidegrid: bool = false
+var overlapping_supplies: Array = []
+var old_position
+var selected = false
 
 
 func _ready() -> void:
 	isready = true
+
+
+func _process(delta) -> void:
+	if selected:
+		global_position = (
+			self.get_global_mouse_position() - Vector2(
+				item_size.x /2, item_size.y/2
+			)
+		).snapped(item_size)
+	if overlapping_supplies.size() > 0:
+		$Item.modulate = Color(0.4, 0, 0)
+	else:
+		$Item.modulate = Color(0, 0, 0)
 
 
 func put_as_supply() -> void:
@@ -44,9 +64,69 @@ func _set_image(value: Texture2D) -> Texture2D:
 func _set_dimension(value: Vector2) -> Vector2:
 	if isready:
 		$Item/Area/Boundary.set_position(
-			Vector2(value.x/2, value.y/2)
+			Vector2((item_size.x * value.x)/2, (item_size.y * value.y)/2)
 		)
 		$Item/Area/Boundary.shape.set_size(
-			Vector2(value.x, value.y)
+			Vector2((item_size.x * value.x), (item_size.y * value.y)) - pixel_to_leave
 		)
 	return value
+
+
+func initiate():
+	connect("gui_input", hover)
+	$Item/Area.connect("area_entered", overlapping_with_other_item)
+	$Item/Area.connect("area_exited", not_overlapping_with_other_item)
+
+
+func inside_inventory(area: Area2D) -> void:
+	isinsidegrid = true
+	
+func outside_inventory(area: Area2D) -> void:
+	isinsidegrid = false
+
+
+func hover(event: InputEvent) -> void:
+	if event.is_action_pressed("select_item"):
+		selected = true
+#		bl_IsItemSelected = true
+#		ctrl_SelectedItem = ctrl_Item
+		$Item.set_z_index(1000)
+		old_position = global_position
+			
+	if event.is_action_released("select_item"):
+		selected = false
+		$Item.set_z_index(0)
+		if overlapping_supplies.size() > 0:
+			global_position = old_position
+#			ctrl_SelectedItem.get_node("Sprite2D").modulate = col_ValidColor
+		else:
+			if isinsidegrid: 
+				if !get_tree().get_first_node_in_group('supplygrid').add_item_to_inventory(self):
+					global_position = old_position
+				else:
+					pass
+			
+#		bl_IsDraggingItem = false
+#		ctrl_SelectedItem = null
+
+
+func overlapping_with_other_item(area: Area2D) -> void:
+	var supply = area.get_parent().get_parent()
+	if supply == self:
+		return
+	
+	if area == get_tree().get_first_node_in_group('supplygrid'):
+		return
+	if supply.is_in_group('supplyitem'):
+		overlapping_supplies.append(supply)
+
+
+func not_overlapping_with_other_item(area: Area2D) -> void:
+	var supply = area.get_parent().get_parent()
+	if supply == self:
+		return
+	
+	if area == get_tree().get_first_node_in_group('supplygrid'):
+		return
+		
+	overlapping_supplies.erase(supply)
